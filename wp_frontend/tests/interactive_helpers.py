943ecc42659edd21efeba7f539b20f28703a7988
Thomas Bach <bachth@uni-mainz.de>
2011-11-03 20:25:16 +0100

    create a testapp

--- wp_frontend/tests/interactive_helpers.py
+++ wp_frontend/tests/interactive_helpers.py
@@ -2,8 +2,16 @@
 
 from wp_frontend import tests
 from wp_frontend.tests import create_entries
+from webtest import TestApp
+import wp_frontend
 
 tests.createEngineAndInitDB()
+
+def make_testapp():
+    app = wp_frontend.main({}, 
+                           sql_init_function=tests.init_testing_db, 
+                           **tests.settings)
+    return TestApp(app)
 
 def create_all_entries():
     create_entries.add_get_data_entries_to_db(
