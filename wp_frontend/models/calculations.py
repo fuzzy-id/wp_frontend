b2652b73e82d0c7be1afb72fe9c9b5ed5e2a3588
Thomas Bach <bachth@uni-mainz.de>
2011-11-17 13:54:38 +0100

    added calculation to currKW class

--- wp_frontend/models/calculations.py
+++ wp_frontend/models/calculations.py
@@ -2,17 +2,16 @@
 from wp_frontend.models.get_data import PulledData
 
 
-def calc_currKW(temp_Vl):
-    if temp_Vl <= 40:
-        return (0.06 * temp_Vl) + 0.5
-    elif 40 < temp_Vl < 50:
-        return (0.08 * temp_Vl) - 0.3
-    return (0.1 * temp_Vl) - 1.3
-
 class CurrKW(object):
     name = 'currKW'
     needed_columns = [PulledData.temp_Vl]
-    calc = staticmethod(calc_currKW)
+    @staticmethod
+    def calc(temp_Vl):
+        if temp_Vl <= 40:
+            return (0.06 * temp_Vl) + 0.5
+        elif 40 < temp_Vl < 50:
+            return (0.08 * temp_Vl) - 0.3
+        return (0.1 * temp_Vl) - 1.3
 
 class DeltaVlRl(object):
     name = 'deltaVlRl'
