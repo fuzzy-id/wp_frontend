e5b0756936027381bc04850412d411879fa2efc5
Thomas Bach <bachth@uni-mainz.de>
2012-02-12 15:58:30 +0100

    test needs logged in user

--- wp_frontend/tests/test_functionals/test_status_page.py
+++ wp_frontend/tests/test_functionals/test_status_page.py
@@ -10,6 +10,10 @@ class AuthenticationTests(BasicFunctionalTestCase):
 
 class ViewTests(BasicFunctionalTestCase):
     
+    def setUp(self):
+        super(ViewTests, self).setUp(self)
+        self.login()
+
     def test_status_page_viewable(self):
         res = self.testapp.get('/status', status=200)
         self.assertLoggedIn(res)
