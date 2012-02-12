e92d935e9ae9c515dc8a7e44215d1d9aace590ba
Thomas Bach <bachth@uni-mainz.de>
2012-02-12 14:28:06 +0100

    adding template

--- wp_frontend/views/status.py
+++ wp_frontend/views/status.py
@@ -2,6 +2,7 @@
 
 from pyramid.view import view_config
 
-@view_config(route_name="view_status", permission='user')
+@view_config(route_name="view_status", permission='user',
+             renderer=os.path.join(settings.templates_dir, 'status.pt'))
 def view_status(request):
     pass
