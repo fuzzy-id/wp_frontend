8b627e9b6629bd19fe20c0b340fcfaf1faa3afec
Thomas Bach <bachth@uni-mainz.de>
2012-02-12 16:55:08 +0100

    added view for backup

--- wp_frontend/views/status.py
+++ wp_frontend/views/status.py
@@ -9,3 +9,8 @@ from pyramid.view import view_config
              renderer=os.path.join(settings.templates_dir, 'status.pt'))
 def view_status(request):
     return {}
+
+@view_config(route_name="view_backup", permission='user',
+             renderer=os.path.join(settings.templates_dir, 'status.pt'))
+def view_backup(request):
+    return {}
