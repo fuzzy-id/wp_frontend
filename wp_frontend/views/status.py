482cc976055ec7cf3928c2e3c650279f4ef6eaa5
Thomas Bach <bachth@uni-mainz.de>
2012-02-12 14:32:35 +0100

    typo

--- wp_frontend/views/status.py
+++ wp_frontend/views/status.py
@@ -3,7 +3,7 @@
 import os.path
 
 from wp_frontend import settings
-from pyramid.view import settings
+from pyramid.view import view_config
 
 @view_config(route_name="view_status", permission='user',
              renderer=os.path.join(settings.templates_dir, 'status.pt'))
