2f90af34c7a2fb12f682720ffada49e2c5d75afe
Thomas Bach <bachth@uni-mainz.de>
2012-02-13 23:07:39 +0100

    defining backup template

--- wp_frontend/models/backup.py
+++ wp_frontend/models/backup.py
@@ -1,1 +1,13 @@
 # -*- coding: utf-8 -*-
+
+from sqlalchemy import Column, Integer, Sequence, String, Text
+
+from wp_frontend.models import Base
+
+class BackupTemplate(Base):
+
+    __tablename__ = 'backup_template'
+    id = Column(Integer, Sequence('template_id'), primary_key=True)
+    name = Column(String(20))
+    root = Column(String(40))
+    exclude = Column(Text)
